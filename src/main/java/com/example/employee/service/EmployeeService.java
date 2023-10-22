8bf4bb30f7461ab6f29be4643b7149f80cacb9ee
Amila De Silva <jaadds@gmail.com>
2023-10-22 10:53:44 +0530

    Adding employee request

--- src/main/java/com/example/employee/service/EmployeeService.java
+++ src/main/java/com/example/employee/service/EmployeeService.java
@@ -2,9 +2,11 @@ package com.example.employee.service;
 
 import com.example.employee.model.Employee;
 import com.example.employee.model.EmployeeRepository;
+import com.example.employee.model.EmployeeRequest;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 
 
@@ -14,8 +16,13 @@ public class EmployeeService {
     @Autowired
     private EmployeeRepository employeeRepository;
 
-    public Employee createEmployee(Employee employee) {
-        return employeeRepository.save(employee);
+    public Employee createEmployee(EmployeeRequest employee) {
+        Employee newEmployee = new Employee();
+        newEmployee.setFirstName(employee.firstName());
+        newEmployee.setLastName(employee.lastName());
+        newEmployee.setEmail(employee.email());
+
+        return employeeRepository.save(newEmployee);
     }
 
     public List<Employee> getEmployees() {
@@ -26,8 +33,14 @@ public class EmployeeService {
         employeeRepository.deleteById(employeeId);
     }
 
-    public Employee updateEmployee(Employee employee) {
-        return employeeRepository.save(employee);
+    public Employee updateEmployee(int employeeId, EmployeeRequest employee) {
+        Employee updatedEmployee = new Employee();
+        updatedEmployee.setId(employeeId);
+        updatedEmployee.setFirstName(employee.firstName());
+        updatedEmployee.setLastName(employee.lastName());
+        updatedEmployee.setEmail(employee.email());
+        
+        return employeeRepository.save(updatedEmployee);
     }
     
 }
