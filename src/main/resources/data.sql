1a3895c780b9ca6b10efc8b345637f6cb85e619e
Amila De Silva <jaadds@gmail.com>
2023-11-07 06:49:52 +0530

    Executing data.sql only once

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,3 @@
--- CREATE TABLE IF NOT EXISTS EMPLOYEE (
---     EMP_ID INT AUTO_INCREMENT PRIMARY KEY,
---     FIRST_NAME VARCHAR(255) NOT NULL,
---     LAST_NAME VARCHAR(255),
---     EMAIL VARCHAR(255)
--- );
-
--- INSERT INTO EMPLOYEE(FIRST_NAME, LAST_NAME, EMAIL) VALUES ('Bilbo', 'Baggins', 'bilbo@bagend.me');
-INSERT INTO employee(first_name, last_name, email) VALUES ('Bilbo', 'Baggins', 'bilbo@bagend.me');
-
-
 INSERT INTO employee(first_name, last_name, email)
 SELECT 'Bilbo','Baggins', 'bilbo@bagend.me'
 FROM dual
